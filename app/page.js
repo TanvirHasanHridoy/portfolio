605e20299bed2b24cc2996cd592a3ae3473251a8
Hridoy <th.hridoy2013@gmail.com>
2023-10-12 19:57:36 +0200

    Base project
    this is just to setting up the project

--- app/page.js
+++ app/page.js
@@ -1,3 +1,9 @@
+
+
 export default function Home() {
-  return <main className=""></main>;
+  return <main className="">
+  <section className="min-h-min">
+  This will be the body of the page
+  </section>
+  </main>;
 }
